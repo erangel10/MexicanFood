0553d8751270f56b7b4a408e6fed18d3452b2088
erangel10 <rangeleduardo10@gmail.com>
2016-02-13 20:11:02 -0800

    Update CreativeTabBlocks.java

--- 1.8.8 Food Mod/src/main/java/com/rako100/creativetabs/CreativeTabBlocks.java
+++ 1.8.8 Food Mod/src/main/java/com/rako100/creativetabs/CreativeTabBlocks.java
@@ -3,9 +3,7 @@ package com.rako100.creativetabs;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
-/**
- * Created by EDUARDO on 1/31/2016.
- */
+
 public class CreativeTabBlocks extends CreativeTabs{
 
     public CreativeTabBlocks(String lable)
